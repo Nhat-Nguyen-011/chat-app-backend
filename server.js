f042b6dde94a56f58204de0a83c186338198bf5b
Nhat-Nguyen-011 <nhat.nguyen1125@gmail.com>
2020-09-01 21:08:42 +0700

    yas

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 const authRoutes = require('./Routes/authRoutes');
 
-// app.use('/auth', authRoutes);
+app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send('hello world');
